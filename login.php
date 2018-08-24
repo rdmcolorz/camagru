b4866576d6945d7fa6586c9031ba6aef4c2a2b75
42 Robotics <42robotics@maccheese.local>
2018-08-24 16:22:57 -0700

    stuff

--- login.php
+++ login.php
@@ -1,59 +1,66 @@
 <?php
-   include("config.php");
-   session_start();
+    include("config.php");
+    session_start();
    
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
+    if($_SERVER["REQUEST_METHOD"] == "POST") 
+    {
+        // username and password sent from form 
       
-      // escapes special characters in the string.
-      $myusername = mysqli_real_escape_string($db,$_POST['username']);
-      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
+        // escapes special characters in the string.
+        $myusername = mysqli_real_escape_string($db,$_POST['username']);
+        $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
       
-      $sql = "SELECT id FROM user_data WHERE login = '$myusername' and passwd= '$mypassword'";
-      $result = mysqli_query($db,$sql);
-      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $active = $row['active'];
-      $count = mysqli_num_rows($result);
+        $sql = "SELECT id FROM user_data WHERE login = '$myusername' and passwd= '$mypassword'";
+        $result = mysqli_query($db,$sql);
+        $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+        echo $row;
+        $active = $row['active'];
+        $count = mysqli_num_rows($result);
+        $error = "";
       
-      // If result matched $myusername and $mypassword, table row must be 1 row
-		
-      if($count == 1) {
-         session_register("myusername");
-         $_SESSION['login_user'] = $myusername;
-
-         header("location: welcome.php");
-      } else {
-         $error = "Your Login Name or Password is invalid";
-      }
-   }
+        // If result matched $myusername and $mypassword, table row must be 1 row
+        if($count == 1) 
+        {
+            session_register("myusername");
+            $_SESSION['login_user'] = $myusername;
+            header("Location: welcome.php");
+        } 
+        else 
+        {
+            $error = "Your Login Name or Password is invalid";
+        }
+    }
 ?>
 
 <html>
    <head>
       <title>Login Page</title>
       <style type = "text/css">
-         body {
+         body 
+         {
             font-family: Arial, Helvetica, sans-serif;
             font-size: 14px;
          }
-         label {
+         label 
+         {
             font-weight: bold;
             width: 100px;
             font-size: 14px;
          }
-         .box {
+         .box 
+         {
             border: #666666 solid 1px;
          }
       </style>
    </head>
-   <body bgcolor = "#FFFFFF">
+   <body bgcolor = "skyblue">
       <div align = "center">
          <div style = "width:300px; border: solid 1px #333333; " align = "left">
             <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
             <div style = "margin:30px">
                <form action = "" method = "post">
-                  <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
-                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
+                  <label>UserName  : </label><input type = "text" name = "username" class = "box"/><br /><br />
+                  <label>Password  : </label><input type = "password" name = "password" class = "box" /><br/><br />
                   <input type = "submit" value = " Submit "/><br />
                </form>
                <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
--- welcome.php
+++ welcome.php
@@ -1,5 +1,13 @@
+<?php
+   include('session.php');
+?>
 <html>
-<head>
-Welcome to the site.
-</head>
+    <head>
+        <title>Welcome </title>
+    </head>
+   
+    <body>
+        <h1>Welcome <?php echo $login_session; ?></h1> 
+        <h2><a href = "logout.php">Sign Out</a></h2>
+    </body>
 </html>
